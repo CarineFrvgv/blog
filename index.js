d631fb8047ff8b11f66722725995dd2e1ec7f23b
CarineFrvgv <93139372+CarineFrvgv@users.noreply.github.com>
2021-11-04 13:53:14 -0300

    Set ejs

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
+const ejs = require('ejs');
 const app = express();
 
 app.use(express.static('public'));
-
+app.set('view engine', 'ejs');
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Listening to port ${PORT}`);
